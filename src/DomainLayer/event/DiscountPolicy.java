3012f7b09e758d564835c31cea2d5a9364192d2c
guettarotem <gurttar@post.bgu.ac.il>
2026-04-19 16:43:02 +0300

    close #2

--- src/DomainLayer/event/DiscountPolicy.java
+++ src/DomainLayer/event/DiscountPolicy.java
@@ -1,5 +1,16 @@
 package DomainLayer.event;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class DiscountPolicy {
-    
+    private List<DiscountTypes> discounts;
+
+    public DiscountPolicy() {
+        this.discounts = new ArrayList<>();
+    }
+    public List<DiscountTypes> getDiscounts() {
+        return discounts;
+    }
 }
