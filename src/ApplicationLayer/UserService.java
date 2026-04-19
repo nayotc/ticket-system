bb3a89773fff871e8cc8affadc6e817ded904f9e
reut-ro <reutrothfeld@gmail.com>
2026-04-19 12:41:44 +0300

    implement UserService with signUp and logIn methods

--- src/ApplicationLayer/UserService.java
+++ src/ApplicationLayer/UserService.java
@@ -1,5 +1,19 @@
 package ApplicationLayer;
 
 public class UserService {
-    
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public void signUp(sessionToken, string username, string password){
+
+    }
+
+    public void logIn(sessionToken, string username, string password){
+
+    }
+
 }
+
