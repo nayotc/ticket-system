1a3abfe4371f1edd3316de810a1edada515e5a13
noaambar <noaamb@post.bgu.ac.il>
2026-04-19 16:58:39 +0300

    finish inital company

--- src/DomainLayer/company/Company.java
+++ src/DomainLayer/company/Company.java
@@ -1,5 +1,47 @@
 package DomainLayer.company;
 
 public class Company {
-    
+    private String name;
+    private Boolean isActive;
+    private PurchasePolicy purchasePolicy;
+    private DiscountPolicy discountPolicy;
+
+    public Company(String name, Boolean isActive, PurchasePolicy purchasePolicy,DiscountPolicy discountPolicy)
+    {
+        this.name=name;
+        this.isActive=isActive;
+        this.discountPolicy=discountPolicy;
+        this.purchasePolicy=purchasePolicy;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public Boolean getIsActive()
+    {
+        return this.isActive;
+    }
+    public void setIsActive(boolean isActive)
+    {
+        this.isActive=isActive;
+    }
+        public void setPurchasePolicy(PurchasePolicy purchasePolicy) {
+        this.purchasePolicy = purchasePolicy;
+    }
+
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
+    public PurchasePolicy getPurchasePolicy() {
+        return purchasePolicy;
+    }
+
+    public DiscountPolicy getDiscountPolicy() {
+        return discountPolicy;
+    }
 }
