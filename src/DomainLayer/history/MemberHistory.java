8315d37df303514d5610786e5542b842bc390c65
TheMit23 <amittami@post.bgu.ac.il>
2026-04-19 17:40:23 +0300

    history with fields

---
+++ src/DomainLayer/history/MemberHistory.java
@@ -0,0 +1,33 @@
+package DomainLayer.history;
+
+import DomainLayer.order.Purchase;
+import DomainLayer.order.Ticket;
+import java.util.List;
+
+public class MemberHistory {
+    private int id;
+
+    public MemberHistory(int id) {
+        this.id = id;
+    }
+    public List<Ticket> getHistoryTickets() {
+        return null;
+    }
+
+    public void addHistoryTicket(Ticket ticket) {
+    }
+
+    public void removeHistoryTicket(Ticket ticket) {
+    }
+
+    public List<Purchase> getHistoryPurchases() {
+        return null;
+    }
+
+    public void addHistoryPurchase(Purchase purchase) {
+    }
+
+    public void removeHistoryPurchase(Purchase purchase) {
+    }
+
+}
