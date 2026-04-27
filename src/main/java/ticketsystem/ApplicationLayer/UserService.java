7c393492de020672be7935778ccc6bff44c874b7
TheMit23 <amittami@post.bgu.ac.il>
2026-04-27 15:16:32 +0300

    added sign up logic

--- src/main/java/ticketsystem/ApplicationLayer/UserService.java
+++ src/main/java/ticketsystem/ApplicationLayer/UserService.java
@@ -1,7 +1,10 @@
 package ticketsystem.ApplicationLayer;
 
+import java.security.SecureRandom;
+
 import ticketsystem.DomainLayer.IRepository.IUserRepository;
 import ticketsystem.DomainLayer.user.Guest;
+import ticketsystem.DomainLayer.user.Member;
 
 public class UserService {
     private final IUserRepository userRepository;
@@ -15,14 +18,26 @@ public class UserService {
     public String visitSystem() {
         Guest guest = new Guest();
         String sessionToken = tokenService.generateNewGuestToken();
-        while (!userRepository.addGuest(sessionToken, guest)) {
+        while (!userRepository.addActiveSession(sessionToken, guest)) {
             sessionToken = tokenService.generateNewGuestToken();
         }
         return sessionToken;
     }
 
     public void signUp(String sessionToken, String username, String password) {
-
+        if(!tokenService.validateToken(sessionToken)) {
+            System.out.println("Invalid session token");
+            return;
+        }
+        if(userRepository.isUsernameTaken(username)) {
+            System.out.println("Username is already taken");
+            return;
+        }
+        Long newId = new SecureRandom().nextLong();
+        while (userRepository.isIDTaken(newId)) {
+            newId = new SecureRandom().nextLong();
+        }
+        userRepository.addRegisteredMember(newId, new Member(newId, username, password));
     }
 
     public void logIn(String sessionToken, String username, String password) {
