19100cd3638e35c3abf5a73c29d453e13663b007
Liat <liatliatgt@gmail.com>
2026-04-30 16:36:16 +0300

    adding skeleton for use case of deleete a member by admin

--- src/main/java/ticketsystem/ApplicationLayer/SystemAdminService.java
+++ src/main/java/ticketsystem/ApplicationLayer/SystemAdminService.java
@@ -1,19 +1,33 @@
 package ticketsystem.ApplicationLayer;
 
+import ticketsystem.DomainLayer.IRepository.ICompanyRepository;
+import ticketsystem.DomainLayer.IRepository.IOrderRepository;
 import ticketsystem.DomainLayer.IRepository.ISystemAdminRepository;
+import ticketsystem.DomainLayer.IRepository.IUserRepository;
 
 public class SystemAdminService {
 
     private final ISystemAdminRepository adminRepository;
     private final IPaymentService paymentService;
     private final ISecureBarcode barcodeService;
+    private final ITokenService tokenService = new TokenService();
+    private final IUserRepository userRepository;
+    private final ICompanyRepository companyRepository;
+    private final IOrderRepository orderRepository;
 
     public SystemAdminService(ISystemAdminRepository adminRepository,
             IPaymentService paymentService,
-            ISecureBarcode barcodeService) {
+            ISecureBarcode barcodeService,
+            IUserRepository userRepository,
+            ICompanyRepository companyRepository,
+            IOrderRepository orderRepository) {
         this.adminRepository = adminRepository;
         this.paymentService = paymentService;
         this.barcodeService = barcodeService;
+        this.userRepository = userRepository;
+        this.companyRepository = companyRepository;
+        this.orderRepository = orderRepository;
+
     }
 
 //Use Case: Ticket System Initialization
@@ -44,6 +58,28 @@ public class SystemAdminService {
         } catch (Exception e) {
             System.out.println("Unexpected error during initialization: " + e.getMessage());
             return false;
+        }
+    }
+
+    // Use-case: Delete Member by Admin
+    public String deleteMemberByAdmin(String sessionId, long memberId) {
+
+        if (!tokenService.validateToken(sessionId)) {
+            return "ERROR: Unauthorized access. Admin is not logged in or lacks permissions.";
+        }
+
+        // Member member = userRepository.getMemberById(memberId);
+        // if (member == null) {
+        //     return "ERROR: Member with ID " + memberId + " was not found.";
+        // }
+        try {
+            //companyRepository.removeRolesForMember(memberId);
+            // orderRepository.cancelPendingOrdersForMember(memberId);
+            // userRepository.removeRegisteredMember(member););
+            return "SUCCESS: Member deactivated and associated records cleaned up.";
+
+        } catch (Exception e) {
+            return "ERROR: An unexpected error occurred while deleting the member: " + e.getMessage();
         }
     }
 
