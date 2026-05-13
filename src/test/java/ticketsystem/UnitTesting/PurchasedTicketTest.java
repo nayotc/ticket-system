d20ecf0719d88f7b04f1e33caeda0b7c1e44f4ab
reut-ro <reutrothfeld@gmail.com>
2026-05-13 12:37:00 +0300

    test changes to fit the new PurchaseDTO

--- src/test/java/ticketsystem/UnitTesting/PurchasedTicketTest.java
+++ src/test/java/ticketsystem/UnitTesting/PurchasedTicketTest.java
@@ -11,7 +11,7 @@ public class PurchasedTicketTest {
     @Test
     public void GivenNewTicket_WhenCreated_ThenStatusIsActive() {
         // Arrange & Act (Setup & Invocation)
-        PurchasedTicket ticket = new PurchasedTicket(1, 101, 1, 1, 150.0, "SECURE-BARCODE-123");
+        PurchasedTicket ticket = new PurchasedTicket(1, 1, 1, 150.0, "SECURE-BARCODE-123");
 
         // Assert (Assessment)
         assertEquals(TicketStatus.ACTIVE, ticket.getStatus(), "New ticket status should be ACTIVE by default");
