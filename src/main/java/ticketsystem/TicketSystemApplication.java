d72483e26ee92f0379b5f8acf3a4af28e4646ce6
guettarotem <158735787+guettarotem@users.noreply.github.com>
2026-05-19 19:17:25 +0300

    Add Spring Boot and Vaadin initial setup

---
+++ src/main/java/ticketsystem/TicketSystemApplication.java
@@ -0,0 +1,11 @@
+package ticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TicketSystemApplication.class, args);
+    }
+}
