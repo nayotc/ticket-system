bcb6bc2341edd6efa057d7ee25399864bc4b5a8b
nayotc <nayot@post.bgu.ac.il>
2026-05-26 02:31:22 +0300

    Add null checks for company ID and event in calculateFinalPrice method

--- src/main/java/ticketsystem/DomainLayer/EventCatalogDomainService.java
+++ src/main/java/ticketsystem/DomainLayer/EventCatalogDomainService.java
@@ -65,7 +65,8 @@ public class EventCatalogDomainService {
     }
     
      public BigDecimal calculateFinalPrice(Long companyId,Event event,BigDecimal totalPrice,int ticketCount,String couponCode) {
-         if (event == null) throw new IllegalArgumentException("Event cannot be null");
+        if (companyId == null)  throw new IllegalArgumentException("Company id cannot be null");
+        if (event == null) throw new IllegalArgumentException("Event cannot be null");
         if (totalPrice == null) throw new IllegalArgumentException("Total price cannot be null");
         if (ticketCount < 0) throw new IllegalArgumentException("Ticket count cannot be negative");
            Company company=companyRepository.findById(companyId).orElseThrow(() -> new IllegalArgumentException("Company not found"));
