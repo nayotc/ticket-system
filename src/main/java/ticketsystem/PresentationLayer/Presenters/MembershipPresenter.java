2895e812262c07db8146ceff69faa9b0504412c5
rotemfeder <federr@post.bgu.ac.il>
2026-05-29 00:52:15 +0300

    Fix compilation errors

--- src/main/java/ticketsystem/PresentationLayer/Presenters/MembershipPresenter.java
+++ src/main/java/ticketsystem/PresentationLayer/Presenters/MembershipPresenter.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import ticketsystem.ApplicationLayer.CompanyService;
