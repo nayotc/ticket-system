cf268e089fd63bc55390a2f551e54ab6508f50ac
nayotc <nayot@post.bgu.ac.il>
2026-06-09 17:38:29 +0300

    enhance PaymentDetails class with additional fields and constructors for payment processing

--- src/main/java/ticketsystem/DTO/PaymentDetails.java
+++ src/main/java/ticketsystem/DTO/PaymentDetails.java
@@ -1,40 +1,166 @@
+// package ticketsystem.DTO;
+
+// import java.time.LocalDate;
+
+// public class PaymentDetails {
+    
+    
+//     private String paymentMethodId; // Stripe / Fake
+//     private String payerName;
+//     private LocalDate birthDate;
+
+//     public PaymentDetails() {
+//     }
+
+//     public PaymentDetails(String paymentMethodId, String payerName, LocalDate birthDate) {
+//         this.paymentMethodId = paymentMethodId;
+//         this.payerName = payerName;
+//         this.birthDate = birthDate;
+//     }
+    
+//     public String getPaymentMethodId() {
+//         return paymentMethodId;
+//     }
+
+//     public String getPayerName() {
+//         return payerName;
+//     }
+//     public LocalDate getBirthDate() {
+//         return birthDate;
+//     }
+//     public void setPaymentMethodId(String paymentMethodId) {
+//         this.paymentMethodId = paymentMethodId;
+//     }
+//     public void setPayerName(String payerName) {
+//         this.payerName = payerName;
+//     }
+//      public void setBirthDate(LocalDate birthDate) {
+//         this.birthDate = birthDate;
+//     }
+// }
 package ticketsystem.DTO;
 
 import java.time.LocalDate;
 
 public class PaymentDetails {
-    
-    
-    private String paymentMethodId; // Stripe / Fake
+
+    private String paymentMethodId;
+
     private String payerName;
+
     private LocalDate birthDate;
+
+    private String cardNumber;
+
+    private int expirationMonth;
+
+    private int expirationYear;
+
+    private String cvv;
+
+    private String holderId;
+
+    private String currency;
 
     public PaymentDetails() {
     }
-
+   
     public PaymentDetails(String paymentMethodId, String payerName, LocalDate birthDate) {
         this.paymentMethodId = paymentMethodId;
         this.payerName = payerName;
         this.birthDate = birthDate;
     }
-    
+
+    public PaymentDetails(String paymentMethodId,
+                          String payerName,
+                          LocalDate birthDate,
+                          String cardNumber,
+                          int expirationMonth,
+                          int expirationYear,
+                          String cvv,
+                          String holderId,
+                          String currency) {
+
+        this.paymentMethodId = paymentMethodId;
+        this.payerName = payerName;
+        this.birthDate = birthDate;
+        this.cardNumber = cardNumber;
+        this.expirationMonth = expirationMonth;
+        this.expirationYear = expirationYear;
+        this.cvv = cvv;
+        this.holderId = holderId;
+        this.currency = currency;
+    }
+
     public String getPaymentMethodId() {
         return paymentMethodId;
+    }
+
+    public void setPaymentMethodId(String paymentMethodId) {
+        this.paymentMethodId = paymentMethodId;
     }
 
     public String getPayerName() {
         return payerName;
     }
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-    public void setPaymentMethodId(String paymentMethodId) {
-        this.paymentMethodId = paymentMethodId;
-    }
+
     public void setPayerName(String payerName) {
         this.payerName = payerName;
     }
-     public void setBirthDate(LocalDate birthDate) {
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public int getExpirationMonth() {
+        return expirationMonth;
+    }
+
+    public void setExpirationMonth(int expirationMonth) {
+        this.expirationMonth = expirationMonth;
+    }
+
+    public int getExpirationYear() {
+        return expirationYear;
+    }
+
+    public void setExpirationYear(int expirationYear) {
+        this.expirationYear = expirationYear;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getHolderId() {
+        return holderId;
+    }
+
+    public void setHolderId(String holderId) {
+        this.holderId = holderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
     }
 }
