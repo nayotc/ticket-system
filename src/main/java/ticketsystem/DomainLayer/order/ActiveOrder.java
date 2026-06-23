81fee762c1e0b320d70e940849a315a439d60097
nayotc <nayot@post.bgu.ac.il>
2026-06-23 13:22:57 +0300

    fix

--- src/main/java/ticketsystem/DomainLayer/order/ActiveOrder.java
+++ src/main/java/ticketsystem/DomainLayer/order/ActiveOrder.java
@@ -78,7 +78,7 @@ public class ActiveOrder {
         this.userId = userId;
         this.eventId = eventId;
         this.status = OrderStatus.ACTIVE;
-        this.expiresAt = LocalDateTime.now().plusMinutes(1);
+        this.expiresAt = LocalDateTime.now().plusMinutes(15);
     }
 
     public ActiveOrder(ActiveOrder other) {
