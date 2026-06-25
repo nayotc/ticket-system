7276e45da7bde27d3d8596c04d9de514fc9062f7
nayotc <nayot@post.bgu.ac.il>
2026-06-25 21:33:18 +0300

    add time defult

--- src/main/java/ticketsystem/TicketSystemApplication.java
+++ src/main/java/ticketsystem/TicketSystemApplication.java
@@ -1,5 +1,8 @@
 package ticketsystem;
 
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +14,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class TicketSystemApplication {
     public static void main(String[] args) {
+        //for vm
+        System.setProperty("user.timezone", "Asia/Jerusalem");
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Jerusalem"));
+        //for check
+        System.out.println("timezone = " + TimeZone.getDefault().getID());
+        System.out.println("now = " + LocalDateTime.now());
         SpringApplication.run(TicketSystemApplication.class, args);
     }
 
